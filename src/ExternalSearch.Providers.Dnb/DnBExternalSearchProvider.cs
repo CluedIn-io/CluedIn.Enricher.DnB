a12ad59ed3d0396984d5fd8bbd069ea870fe2866
Evropi Parlama <25665777+evropiparlama@users.noreply.github.com>
2024-10-02 01:35:10 +0200

    fix: blockIds parameter casing (#6)

--- src/ExternalSearch.Providers.Dnb/DnBExternalSearchProvider.cs
+++ src/ExternalSearch.Providers.Dnb/DnBExternalSearchProvider.cs
@@ -144,7 +144,7 @@ namespace CluedIn.ExternalSearch.Providers.DnB
                 }
                 else if (!string.IsNullOrWhiteSpace(jobData.BlockIds))
                 {
-                    request.AddQueryParameter("blockIds", jobData.BlockIds);
+                    request.AddQueryParameter("blockIDs", jobData.BlockIds);
                 }
                 else
                 {
