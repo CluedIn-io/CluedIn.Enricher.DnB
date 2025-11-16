b655dbf4d4971b011ede47e5f4528037a64ca50a
Yi-Wei <86753236+ywei390@users.noreply.github.com>
2025-11-16 14:04:40 +0800

    fix: Resolve null exception issues (#25)

--- src/ExternalSearch.Providers.Dnb/DnBExternalSearchProvider.cs
+++ src/ExternalSearch.Providers.Dnb/DnBExternalSearchProvider.cs
@@ -169,7 +169,7 @@ public class DnBExternalSearchProvider : ExternalSearchProviderBase, IExtendedEn
                 throw new ApplicationException("Could not execute external search query - DnB returned empty data");
             }
 
-            var organization = data.organization ?? data.embeddedProduct.organization;
+            var organization = data.organization ?? data.embeddedProduct?.organization;
 
             if (organization == null)
             {
@@ -177,7 +177,7 @@ public class DnBExternalSearchProvider : ExternalSearchProviderBase, IExtendedEn
             }
 
             // Not using industry codes from extended match API
-            if (data.embeddedProduct.organization != null)
+            if (data.embeddedProduct?.organization != null)
             {
                 if (organization.industryCodes != null && organization.industryCodes.Any())
                 {
@@ -520,13 +520,13 @@ public class DnBExternalSearchProvider : ExternalSearchProviderBase, IExtendedEn
 
     private static void PopulateConfidenceScore(IEntityMetadata metadata, IExternalSearchQueryResult<DNBResponse> resultItem)
     {
-        var matchCandidateConfidenceCode = resultItem.Data.matchCandidates.FirstOrDefault()?.matchQualityInformation?.confidenceCode;
+        var matchCandidateConfidenceCode = resultItem.Data?.matchCandidates?.FirstOrDefault()?.matchQualityInformation?.confidenceCode;
         var confidenceScore = 100;
 
         // Match candidate confidence code ranges from 1 (low) to 10 (high).
         // If the value is null or 0, the data is considered to come from the SearchByDUNS API,
         // and the confidence score is set to 100.
-        if (resultItem.Data.matchCandidates.FirstOrDefault() != null && matchCandidateConfidenceCode != null && matchCandidateConfidenceCode != 0)
+        if (matchCandidateConfidenceCode != null && matchCandidateConfidenceCode != 0)
         {
             confidenceScore = (int)matchCandidateConfidenceCode * 10;
         }
